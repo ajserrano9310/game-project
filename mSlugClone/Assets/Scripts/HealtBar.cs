148be0eecc61a0b0e84939c792898a38299cf5b8
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-06-20 18:43:02 -0600

    Day 35: fixing some issues

--- mSlugClone/Assets/Scripts/HealtBar.cs
+++ mSlugClone/Assets/Scripts/HealtBar.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class HealtBar : MonoBehaviour
 {
 
-    //public Slider slider; 
-    
+    public Slider slider;
+    public int num;
+
     public void SetHealth(int health)
     {
 
-      
+        slider.value = health;
 
     }
 }
